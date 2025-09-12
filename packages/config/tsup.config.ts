42d7f7e31c181eca0605ba43a22138306e59f54f
Fabiano Pinto <fabianopinto@gmail.com>
2025-09-12 13:56:08 +0200

    chore: update dependencies and add AWS SDK peer dependencies

--- packages/config/tsup.config.ts
+++ packages/config/tsup.config.ts
@@ -4,7 +4,13 @@ import { baseConfig } from "../../tsup.config";
 export default defineConfig({
   ...baseConfig,
   entry: ["src/index.ts"],
-  external: ["dotenv", "zod", "@fabianopinto/errors"],
+  external: [
+    "@fabianopinto/errors",
+    "@fabianopinto/logger",
+    "@fabianopinto/utils",
+    "@aws-sdk/client-ssm",
+    "@aws-sdk/client-s3",
+  ],
   dts: {
     resolve: true,
     compilerOptions: {
